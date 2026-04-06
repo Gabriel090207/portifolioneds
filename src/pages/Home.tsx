3ea7658b6e079fb4f6004f91a430e235d65d3e24
Gabriel Borela <gabrielborela@MacBook-Air-de-Gabriel.local>
2026-04-06 14:16:12 -0300

    testando responsividade

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import Navbar from "../components/Navbar"
 import Hero from "../components/Hero"
-import Services from "../components/Solutions "
+import Services from "../components/Solutions"
 import Projects from "../components/Projects"
 import Testimonials from "../components/Testimonials"
 import About from "../components/About"
